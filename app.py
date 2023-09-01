537d9eb2560dde019aa013d08e3be1125c19767f
Gyudong Han <bmil2011s@naver.com>
2023-09-01 09:56:51 +0900

    lib_update2

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 import numpy as np
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly as px
 
@@ -14,7 +13,6 @@ def main():
     st.write(np.__version__)
     st.write(st.__version__)
     st.write(pd.__version__)
-    st.write(plt.__version__)
     st.write(sns.__version__)
     st.write(px.__version__)
     
