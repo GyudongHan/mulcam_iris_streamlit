1d68c2b082ec9c211733396a91e9cac863362833
Gyudong Han <bmil2011s@naver.com>
2023-09-01 09:55:31 +0900

    lib_update

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 # -*- coding:utf-8 -*-
 
-import streamlit as st
 import numpy as np
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly as px
+
 
 def main():
 
     st.markdown("# Hello World")
     st.write(np.__version__)
+    st.write(st.__version__)
+    st.write(pd.__version__)
+    st.write(plt.__version__)
+    st.write(sns.__version__)
+    st.write(px.__version__)
+    
 
 if __name__ == "__main__":
     main()
